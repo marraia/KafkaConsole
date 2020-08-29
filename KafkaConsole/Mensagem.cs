3fbb14c2667855a1ecdb8cbd117969160bcd3df8
Fernando Mendes <fernando.abreu@outlook.com>
2020-08-29 09:53:37 -0300

    Novo

---
+++ KafkaConsole/Mensagem.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KafkaConsole
+{
+    public class Mensagem
+    {
+        public SchemaPrimary schema { get; set; }
+        public Payload payload { get; set; }
+    }
+
+    public class SchemaPrimary
+    {
+        public string type { get; set; }
+        public ICollection<FieldsPrimary> fields { get; set; }
+        public bool optional { get; set; }
+        public string name { get; set; }
+    }
+
+    public class FieldsPrimary
+    {
+        public string type { get; set; }
+        public ICollection<FieldChield> fields { get; set; }
+        public bool optional { get; set; }
+        public string field { get; set; }
+        public string name { get; set; }
+    }
+
+    public class FieldChield
+    {
+        public string type { get; set; }
+        public bool optional { get; set; }
+        public string field { get; set; }
+    }
+
+    public class Payload
+    {
+        public Entities before { get; set; }
+        public Entities after { get; set; }
+    }
+
+    public class Entities
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+    }
+
+    public class KeyIndentification
+    {
+        public Identification payload { get; set; }
+    }
+
+    public class Identification
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ KafkaConsole/Program.cs
@@ -0,0 +1,74 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace KafkaConsole
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var consumerConfig = new ConsumerConfig
+            {
+                GroupId = $"dbserver1.dbo.Person.{Guid.NewGuid():N}.group.id",
+                BootstrapServers = "192.168.0.29:9092",
+                AutoOffsetReset = AutoOffsetReset.Earliest,
+            };
+
+
+            using (var c = new ConsumerBuilder<string, string>(consumerConfig).Build())
+            {
+                c.Subscribe("dbserver1.dbo.Person");
+                try
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            var cr = c.Consume();
+                            var a = JsonSerializer.Serialize(cr.Message);
+                            Mensagem b = null;
+
+                            if (cr.Message.Value != null)
+                                b = JsonSerializer.Deserialize<Mensagem>(cr.Message?.Value);
+
+                            var chave = JsonSerializer.Deserialize<KeyIndentification>(cr.Key);
+                            var offset = cr.TopicPartitionOffset;
+
+                            if (b == null)
+                            {
+                                Console.WriteLine($"DELETADO - {chave.payload.Id}");
+                            }
+                            else
+                            {
+                                if (b.payload?.after != null && b.payload?.before == null)
+                                    Console.WriteLine($"INCLUSÃO - ID:{b.payload?.after.Id}\nNOME:{b.payload?.after.Name}\nENDERECO:{b.payload?.after.Address}\nTELEFONE:{b.payload?.after.Phone}");
+                                else
+                                {
+                                    if (b.payload?.after == null)
+                                        Console.WriteLine($"ALTERAÇÃO - ID:{b.payload?.before.Id}\nNOME:{b.payload?.before.Name}\nENDERECO:{b.payload?.before.Address}\nTELEFONE:{b.payload?.before.Phone}");
+                                    else
+                                        Console.WriteLine($"ALTERAÇÃO - ID:{b.payload?.after.Id}\nNOME:{b.payload?.after.Name}\nENDERECO:{b.payload?.after.Address}\nTELEFONE:{b.payload?.after.Phone}");
+                                }
+                            }
+
+                            Console.WriteLine("======================================================== \n");
+
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e);
+                            throw e;
+                        }
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+        }           
+    }
+}
